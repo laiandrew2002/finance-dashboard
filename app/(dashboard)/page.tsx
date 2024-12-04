8559d0cee3018fb6819935871b0516667edf3f9d
Andrew Lai <laiandrew@hotmail.com>
2024-12-04 17:28:13 +0800

    add react-query and query user accounts on hook

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,9 @@
+"use client"
+
+// import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
+
 export default function Home() {
+  // const accountsQuery = useGetAccounts();
   return (
     <div>
       <p>Dashboard page</p>
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,22 +1,24 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import accounts from './accounts';
+import { HTTPException } from 'hono/http-exception';
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api');
 
-app
-  .get('/hello',
-    clerkMiddleware(),
-    async (c) => {
-      const auth = await getAuth(c);
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+  return c.json({ error: "Internal error" }, 500);
+});
 
-      if (!auth?.userId) {
-        return c.json({ error: 'Unauthorized' });
-      }
-      return c.json({ message: 'Hello World!', userId: auth.userId });
-  });
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const routes = app
+  .route('/accounts', accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
